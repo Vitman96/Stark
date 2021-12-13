f8ac26ddd846c02fe23b8776c1dd8dba65181e1f
Vitman96 <41868387+Vitman96@users.noreply.github.com>
2021-12-13 19:46:05 +0100

    Player Style Bug Fix
    change bracket bug for git

--- src/Player.java
+++ src/Player.java
@@ -1,46 +1,40 @@
 import java.util.Scanner;
-public class Player{
+public class Player {
 
     public String name; //Name of Player for UI and Score  Board
     public int cashLevel; //How much Cash has Player won currently
 
     //Creat Player Class with Custom Name
-    public  class Player(String name)
-    {
+    public Player(String name) {
        this.name = name;
        cashLevel = 0;
     }
 
     // Create Default Player Class if Player name is Set
-    public class Player()
-    {
+    public Player() {
         this.name = "Demo Player";
         cashLevel = 0;
     }
 
     // Get Player Name Value
-    public String getName()
-    {
+    public String getName() {
         return  name;
     }
 
     // Get Current cashLevel Value
-    public  int getCashLevel()
-    {
+    public  int getCashLevel() {
         return cashLevel;
     }
 
     // Change cashLevel Value
 
-    public void setCashLevel(int cashLevel)
-    {
+    public void setCashLevel(int cashLevel) {
         this.cashLevel= cashLevel;
     }
 
 
     // Function to get Answer from Player
-    public String getPlayerAnswer()
-    {
+    public String getPlayerAnswer() {
         String answer ="";
         Scanner in = new Scanner(System.in);
         System.out.println("Deine Antwort ist:");
