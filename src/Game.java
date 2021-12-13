5af8a52415617512d8438ad3d0169f5abefcf5ce
Fredonautilus <frederik@prasch.de>
2021-12-13 20:15:14 +0100

    Implemented askQuestion function

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,14 @@
 public class Game{
 
+    /**
+     * Prints out the Question and the choices for answering
+     * @param question
+     */
+    public void askQuestion(Question question){
+        System.out.println(question.getQuestion());
+        System.out.println("A: " + question.getAnswers[0]);
+        System.out.println("B: " + question.getAnswers[1]);
+        System.out.println("C: " + question.getAnswers[2]);
+        System.out.println("D: " + question.getAnswers[3]);
+    }
 }
