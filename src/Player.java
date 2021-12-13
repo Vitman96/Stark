c2106be0088a1f1a09d6ce975bcb9352b6c13b91
Vitman96 <41868387+Vitman96@users.noreply.github.com>
2021-12-13 19:33:42 +0100

    Change Player Class
    - Create Base Player instance Value
    - Create get/ setter funktion for name and cashLevel
    - Create Function to get Answer from Player answer

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
 public class Player{
 
+    public String name; //Name of Player for UI and Score  Board
+    public int cashLevel; //How much Cash has Player won currently
+
+    //Creat Player Class with Custom Name
+    public  class Player(String name)
+    {
+      this.name = name;
+      cashLevel = 0;
+    }
+
+    // Create Default Player Class if Player name is Set
+    public class Player()
+    {
+        this.name = "Demo Player";
+        cashLevel = 0;
+    }
+
+    // Get Player Name Value
+    public String getName()
+    {
+        return  name;
+    }
+
+    // Get Current cashLevel Value
+    public  int getCashLevel()
+    {
+        return cashLevel;
+    }
+
+    // Change cashLevel Value
+
+    public void setCashLevel(int cashLevel)
+    {
+        this.cashLevel= cashLevel;
+    }
+
+
+    // Funktion to get Answer from Player
+    public String getPlayerAnswer()
+    {
+        String answer ="";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Deine Antwort ist:");
+        answer = in.nextLine();
+
+        return answer;
+    }
 }
