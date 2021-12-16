52d8144b2084b8bed570e41afae3e57d246b44b0
*Elvin <*elvin.alirzayev@lt.hs-fulda.de*>
2021-12-16 21:45:56 +0100

    Change char to String

--- src/Question.java
+++ src/Question.java
@@ -2,7 +2,11 @@ public class Question {
 
     private String question;    // The question to be asked
     private String[] answers = new String[4];   // The choices for answering
-    private char rightAnswer;   // The right answer either a, b, c, or d
+    private String rightAnswer;   // The right answer either a, b, c, or d
+
+    public Question() {
+
+    }
 
     public String getQuestion() {
         return this.question;
