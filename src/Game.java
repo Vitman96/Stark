2b4b9d09fc282a5cb1b9cef07a6495ed191126c1
Vitman96 <41868387+Vitman96@users.noreply.github.com>
2021-12-16 22:18:16 +0100

    fix string bug

--- src/Game.java
+++ src/Game.java
@@ -83,7 +83,7 @@ public class Game {
         System.out.println("C: " + question.getAnswers()[2]);
         System.out.println("D: " + question.getAnswers()[3]);
     }
-    ‚
+    
     /**
      * Terminate the Game.
      */
