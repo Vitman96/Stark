fa230b99f80a215115b5580532c1a7c22f7d78c5
Fredonautilus <frederik@prasch.de>
2021-12-16 20:49:39 +0100

    Update Game.java
    Fixed typo

--- src/Game.java
+++ src/Game.java
@@ -7,9 +7,9 @@ public class Game {
      */
     public void askQuestion(Question question) {
         System.out.println(question.getQuestion());
-        System.out.println("A: " + question.getAnswers[0]);
-        System.out.println("B: " + question.getAnswers[1]);
-        System.out.println("C: " + question.getAnswers[2]);
-        System.out.println("D: " + question.getAnswers[3]);
+        System.out.println("A: " + question.getAnswers()[0]);
+        System.out.println("B: " + question.getAnswers()[1]);
+        System.out.println("C: " + question.getAnswers()[2]);
+        System.out.println("D: " + question.getAnswers()[3]);
     }
 }
