e363d26e123581be709e7782b6037a46d4a22a21
Vitman96 <41868387+Vitman96@users.noreply.github.com>
2021-12-16 22:10:18 +0100

    Game Menu
    - change Main(9
    - crearte gameWelcomeMenuPrint´()
    - create gameModeSwitcher()
    - create endGame()
    - create startGame()

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,74 @@
+import java.util.Scanner;
+
 public class Game {
 
 
 
     private boolean gameRunning = true;
 
+
     public static void main(String[] args) {
         Game game = new Game();
-        Player player = new Player();
-        Question[] questions;
+        // MAYBE ERST DANN ERZEUGEN WENN SPIEL GESTARTET WIRD
+        /*Player player = new Player();
+        Question[] questions;*/
 
+
+
+        int menuInputValue = gameWelcomeMenuPrint();
+        gameModeSwitcher(menuInputValue);
+        //START GAME
         while (game.isGameRunning()){
+             // TODO
 
+        }
+    }
+
+    /**
+     * Print the Welcome Text and Game Menu.
+     */
+    public static int gameWelcomeMenuPrint() {
+
+        //GAME WELCOME MENUE
+        System.out.println("############### HERZLICH WILLKOMMEN ZU ################");
+        System.out.println("############### WER WIRD MILLIONÄR? ################");
+        System.out.println("############### BY TEAM STARK ################");
+
+
+        System.out.println("\n\n\nBITTE WÄHLEN SIE AUS");
+        System.out.println("[1]  SPIEL STARTEN");
+        System.out.println("[2]  SCORE BOARD");
+        System.out.println("[3]  SPIEL BEENDEN");
+
+        System.out.print("\n\n\nIHRE EINGABE: ");
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        return  input;
+    }
+
+    /**
+     * Switch between the diffrent Game modes from Menu.
+     *
+     * @param input int
+     */
+    public static void gameModeSwitcher(int input) {
+        switch (input) {
+            case 1:
+                startGame();
+                break;
+            case 2:
+                showScoreBoard();
+                break;
+            default:
+                endGame();
+                break;
         }
     }
 
     public boolean isGameRunning() {
         return gameRunning;
     }
+
 
     /**
      * Prints out the Question and the choices for answering.
@@ -29,5 +81,37 @@ public class Game {
         System.out.println("B: " + question.getAnswers()[1]);
         System.out.println("C: " + question.getAnswers()[2]);
         System.out.println("D: " + question.getAnswers()[3]);
+    }
+
+    /**
+     * Terminate the Game.
+     */
+    public static void endGame() {
+        System.out.print("\n\n\n SPIEL BEENDET");
+    }
+
+
+    /**
+     * Show High Score Board.
+     */
+    public static void showScoreBoard() {
+        System.out.print("\n\n\n SCORE BOARD");
+    }
+
+    /**
+     * Start the Game.
+     */
+    public static void startGame() {
+
+
+        //Get Player Name
+        System.out.print("BITTE GEBEN SIE IHREN NAMEN EIN: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        Player player = new Player(name);
+
+        //Welcome Player with his Name
+        System.out.println("HERZLICH WILLKOMMEN " + player.getName());
+
     }
 }
