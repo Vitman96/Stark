a9c79fd4a88ed7eb4093c165c2f25ed6ccaa14bf
*Elvin <*elvin.alirzayev@lt.hs-fulda.de*>
2021-12-16 21:46:53 +0100

    Add variables

--- src/Question.java
+++ src/Question.java
@@ -3,6 +3,9 @@ public class Question {
     private String question;    // The question to be asked
     private String[] answers = new String[4];   // The choices for answering
     private String rightAnswer;   // The right answer either a, b, c, or d
+    String[] questions;
+    private Scanner sc;
+    private File file;
 
     public Question() {
 
