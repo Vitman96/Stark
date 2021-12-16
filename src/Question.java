260a0a83f2cabb8209559706374e5ad29f5a27a9
TimH810 <94465655+TimH810@users.noreply.github.com>
2021-12-16 21:43:36 +0100

    Compare method for correct answers

--- src/Question.java
+++ src/Question.java
@@ -2,7 +2,7 @@ public class Question {
 
     private String question;    // The question to be asked
     private String[] answers = new String[4];   // The choices for answering
-    private char rightAnswer;   // The right answer either a, b, c, or d
+    private String rightAnswer;   // The right answer either a, b, c, or d
 
     public String getQuestion() {
         return this.question;
@@ -10,5 +10,9 @@ public class Question {
 
     public String[] getAnswers() {
         return this.answers;
+    }
+
+    public String getRightAnswer() {
+        return this.rightAnswer;
     }
 }
